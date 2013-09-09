dda56269f70f58204b69a2b2a4a1c503f525f063
Jim Huang <jserv@0xlab.org>
2013-09-10 00:25:46 +0800

    sfit: syntax tweaks

--- sfit.c
+++ sfit.c
@@ -40,7 +40,7 @@ double abs (double a)
 double sqrt(double x)
 {
 	double i = x/2;
-	if (x<0)
+	if (x < 0)
 		return 0;
 	while (fabs(i - (x / i)) / i >0.000000000000001)
 		i = (i + (x / i)) / 2;
@@ -58,20 +58,17 @@ pts_t *new_pts (void)
 	return pts;
 }
 
-	void
-dispose_pts (pts_t *pts)
+void dispose_pts (pts_t *pts)
 {
 	if (pts->pt)
 		free (pts->pt);
 	free (pts);
 }
 
-	void
-add_pt (pts_t *pts, pt_t *pt)
+void add_pt (pts_t *pts, pt_t *pt)
 {
-	if (pts->n == pts->s)
-	{
-		int	ns = pts->s ? pts->s * 2 : 16;
+	if (pts->n == pts->s) {
+		int ns = pts->s ? pts->s * 2 : 16;
 
 		if (pts->pt)
 			pts->pt = realloc (pts->pt, ns * sizeof (pt_t));
@@ -82,8 +79,7 @@ add_pt (pts_t *pts, pt_t *pt)
 	pts->pt[pts->n++] = *pt;
 }
 
-	double
-distance_to_point (pt_t * a, pt_t * b)
+double distance_to_point (pt_t * a, pt_t * b)
 {
 	double dx = (b->x - a->x);
 	double dy = (b->y - a->y);
@@ -91,8 +87,7 @@ distance_to_point (pt_t * a, pt_t * b)
 	return sqrt (dx*dx + dy*dy);
 }
 
-	double
-distance_to_line (pt_t * p, pt_t * p1, pt_t * p2)
+double distance_to_line (pt_t * p, pt_t * p1, pt_t * p2)
 {
 	/*
 	 * Convert to normal form (AX + BY + C = 0)
@@ -107,10 +102,10 @@ distance_to_line (pt_t * p, pt_t * p1, pt_t * p2)
 	 *
 	 * distance² = (AX + BC + C)² / (A² + B²)
 	 */
-	double   A = p2->y - p1->y;
-	double   B = p1->x - p2->x;
-	double   C = (p1->y * p2->x - p1->x * p2->y);
-	double   den, num;
+	double A = p2->y - p1->y;
+	double B = p1->x - p2->x;
+	double C = (p1->y * p2->x - p1->x * p2->y);
+	double den, num;
 
 	num = A * p->x + B * p->y + C;
 	den = A * A + B * B;
@@ -120,8 +115,7 @@ distance_to_line (pt_t * p, pt_t * p1, pt_t * p2)
 		return sqrt ((num * num) / den);
 }
 
-	double
-distance_to_segment (pt_t * p, pt_t * p1, pt_t * p2)
+double distance_to_segment (pt_t * p, pt_t * p1, pt_t * p2)
 {
 	double dx, dy;
 	double pdx, pdy;
@@ -132,10 +126,9 @@ distance_to_segment (pt_t * p, pt_t * p1, pt_t * p2)
 	   px = p1 + u(p2 - p1)
 	   (p - px) . (p2 - p1) = 0
 
-Thus:
-
-u = ((p - p1) . (p2 - p1)) / (||(p2 - p1)|| ^ 2);
-*/
+	Thus:
+	  u = ((p - p1) . (p2 - p1)) / (||(p2 - p1)|| ^ 2);
+	*/
 
 	dx = p2->x - p1->x;
 	dy = p2->y - p1->y;
@@ -159,38 +152,34 @@ u = ((p - p1) . (p2 - p1)) / (||(p2 - p1)|| ^ 2);
 	return distance_to_point (p, &px);
 }
 
-	double
-distance_to_segments (pt_t * p, pts_t *s)
+double distance_to_segments (pt_t * p, pts_t *s)
 {
 	double  m = distance_to_segment (p, &s->pt[0], &s->pt[1]);
-	int	    i;
-	for (i = 1; i < s->n - 1; i++)
-	{
+	int i;
+	for (i = 1; i < s->n - 1; i++) {
 		double d = distance_to_segment (p, &s->pt[i], &s->pt[i+1]);
 		m = min (d, m);
 	}
 	return m;
 }
 
-	pt_t
-lerp (pt_t *a, pt_t *b)
+pt_t lerp (pt_t *a, pt_t *b)
 {
-	pt_t    p;
+	pt_t p;
 
 	p.x = a->x + (b->x - a->x) / 2;
 	p.y = a->y + (b->y - a->y) / 2;
 	return p;
 }
 
-	void
-dcj (spline_t *s, spline_t *s1, spline_t *s2)
+void dcj (spline_t *s, spline_t *s1, spline_t *s2)
 {
-	pt_t 	ab = lerp (&s->a, &s->b);
-	pt_t 	bc = lerp (&s->b, &s->c);
-	pt_t 	cd = lerp (&s->c, &s->d);
-	pt_t 	abbc = lerp (&ab, &bc);
-	pt_t 	bccd = lerp (&bc, &cd);
-	pt_t 	final = lerp (&abbc, &bccd);
+	pt_t ab = lerp (&s->a, &s->b);
+	pt_t bc = lerp (&s->b, &s->c);
+	pt_t cd = lerp (&s->c, &s->d);
+	pt_t abbc = lerp (&ab, &bc);
+	pt_t bccd = lerp (&bc, &cd);
+	pt_t final = lerp (&abbc, &bccd);
 
 	s1->a = s->a;
 	s1->b = ab;
@@ -216,11 +205,8 @@ double spline_error (spline_t *s)
 void decomp (pts_t *pts, spline_t *s, double tolerance)
 {
 	if (spline_error (s) <= tolerance)
-	{
 		add_pt (pts, &s->a);
-	}
-	else
-	{
+	else {
 		spline_t  s1, s2;
 		dcj (s, &s1, &s2);
 		decomp (pts, &s1, tolerance);
@@ -237,16 +223,13 @@ pts_t *decompose(spline_t *s, double tolerance)
 	return result;
 }
 
-
-	double
-spline_fit_error (pt_t *p, int n, spline_t *s, double tolerance)
+double spline_fit_error (pt_t *p, int n, spline_t *s, double tolerance)
 {
-	pts_t   *sp = decompose (s, tolerance);
-	double  err = 0;
-	int	    i;
+	pts_t *sp = decompose (s, tolerance);
+	double err = 0;
+	int i;
 
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++) {
 		double	e = distance_to_segments (&p[i], sp);
 		err += e * e;
 	}
@@ -254,23 +237,21 @@ spline_fit_error (pt_t *p, int n, spline_t *s, double tolerance)
 	return err;
 }
 
-	pt_t
-lerp_a (pt_t *p1, pt_t *p2, double r)
+pt_t lerp_a (pt_t *p1, pt_t *p2, double r)
 {
-	double  dx = p2->x - p1->x;
-	double  dy = p2->y - p1->y;
-	pt_t    pt;
+	double dx = p2->x - p1->x;
+	double dy = p2->y - p1->y;
+	pt_t pt;
 
 	pt.x = p1->x + r * dx;
 	pt.y = p1->y + r * dy;
 	return pt;
 }
 
-	double
-lerp_dist (pt_t *p1, pt_t *p2, double r)
+double lerp_dist (pt_t *p1, pt_t *p2, double r)
 {
-	double  dx = p2->x - p1->x;
-	double  dy = p2->y - p1->y;
+	double dx = p2->x - p1->x;
+	double dy = p2->y - p1->y;
 
 	dx *= r;
 	dy *= r;
@@ -279,33 +260,31 @@ lerp_dist (pt_t *p1, pt_t *p2, double r)
 
 spline_t fit (pt_t *p, int n)
 {
-	spline_t	s;
-	spline_t	best_s;
+	spline_t s;
+	spline_t best_s;
 
-	double	tol = 0.5;
-	double	best_err = 10000;
-	double	sbx_min;
-	double	sbx_max;
-	double	sby_min;
-	double	sby_max;
-	double	scx_min;
-	double	scx_max;
-	double	scy_min;
-	double	scy_max;
+	double tol = 0.5;
+	double best_err = 10000;
+	double sbx_min;
+	double sbx_max;
+	double sby_min;
+	double sby_max;
+	double scx_min;
+	double scx_max;
+	double scy_min;
+	double scy_max;
 
 	s.a = p[0];
 	s.d = p[n - 1];
 
-	if (s.a.x < s.d.x)
-	{
+	if (s.a.x < s.d.x) {
 		sbx_min = s.a.x;
 		sbx_max = s.d.x;
 
 		scx_max = s.d.x;
 		scx_min = s.a.x;
 	}
-	else
-	{
+	else {
 		sbx_max = s.a.x;
 		sbx_min = s.d.x;
 
@@ -313,16 +292,14 @@ spline_t fit (pt_t *p, int n)
 		scx_max = s.a.x;
 	}
 
-	if (s.a.y < s.d.y)
-	{
+	if (s.a.y < s.d.y) {
 		sby_min = s.a.y;
 		sby_max = s.d.y;
 
 		scy_max = s.d.y;
 		scy_min = s.a.y;
 	}
-	else
-	{
+	else {
 		sby_max = s.a.y;
 		sby_min = s.d.y;
 
@@ -334,12 +311,9 @@ spline_t fit (pt_t *p, int n)
 	for (s.b.x = sbx_min; s.b.x <= sbx_max; s.b.x += 1.0)
 		for (s.b.y = sby_min; s.b.y <= sby_max; s.b.y += 1.0)
 			for (s.c.x = scx_min; s.c.x <= scx_max; s.c.x += 1.0)
-				for (s.c.y = scy_min; s.c.y <= scy_max; s.c.y += 1.0)
-				{
+				for (s.c.y = scy_min; s.c.y <= scy_max; s.c.y += 1.0) {
 					double err = spline_fit_error (p, n, &s, tol);
-
-					if (err < best_err)
-					{
+					if (err < best_err) {
 						best_err = err;
 						best_s = s;
 					}
